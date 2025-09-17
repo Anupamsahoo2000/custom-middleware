9d60cd3df356ae9d035f60865568c2b58220922d
Anupam2000 <anupamsahoo2000@gmail.com>
2025-09-17 13:21:35 +0530

    done

--- index.js
+++ index.js
@@ -2,15 +2,38 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-// Custom middleware
-function addUser(req, res, next) {
-  req.user = "Guest"; // Add new property to req object
-  next(); // Pass control to the next middleware/route handler
-}
+// // Custom middleware
+// function addUser(req, res, next) {
+//   req.user = "Guest"; // Add new property to req object
+//   next(); // Pass control to the next middleware/route handler
+// }
 
-// Route with middleware applied
-app.get("/welcome", addUser, (req, res) => {
-  res.send(`<h1>Welcome, ${req.user}!</h1>`);
+// // Route with middleware applied
+// app.get("/welcome", addUser, (req, res) => {
+//   res.send(`<h1>Welcome, ${req.user}!</h1>`);
+// });
+
+// Middleware to parse JSON body (needed for POST requests)
+app.use(express.json());
+
+// GET /orders
+app.get("/orders", (req, res) => {
+  res.send("Here is the list of all orders.");
+});
+
+// POST /orders
+app.post("/orders", (req, res) => {
+  res.send("A new order has been created.");
+});
+
+// GET /users
+app.get("/users", (req, res) => {
+  res.send("Here is the list of all users.");
+});
+
+// POST /users
+app.post("/users", (req, res) => {
+  res.send("A new user has been added.");
 });
 
 // Start server
