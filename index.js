0d6f8b89146cf10f2c6b7826db5e0a7cdc22137b
Anupam2000 <anupamsahoo2000@gmail.com>
2025-09-19 13:08:34 +0530

    done

--- index.js
+++ index.js
@@ -60,12 +60,36 @@ app.use(express.json());
 //   res.status(404).send("<h1>404 - Page Not Found</h1>");
 // });
 
-app.get("/welcome/:userid", (req, res) => {
-  const id = req.params.userid;
-  const name = req.query.name;
-  const role = req.query.role;
+// app.get("/welcome/:userid", (req, res) => {
+//   const id = req.params.userid;
+//   const name = req.query.name;
+//   const role = req.query.role;
 
-  res.send(`Welcome ${id}-${name}-${role}`);
+//   res.send(`Welcome ${id}-${name}-${role}`);
+// });
+
+// Middleware for logging
+app.use((req, res, next) => {
+  console.log(`${req.method} request made to ${req.url}`);
+  next();
+});
+
+// Routes for products
+app.get("/products", (req, res) => {
+  res.send("Here is the list of all products.");
+});
+
+app.post("/products", (req, res) => {
+  res.send("A new product has been added.");
+});
+
+// Routes for categories
+app.get("/categories", (req, res) => {
+  res.send("Here is the list of all categories.");
+});
+
+app.post("/categories", (req, res) => {
+  res.send("A new category has been created.");
 });
 
 // Start server
